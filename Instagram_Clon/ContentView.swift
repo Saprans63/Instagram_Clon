a5035e713f39244116b44922da43ad8ad90a497f
Azamjon <84767444+Saprans63@users.noreply.github.com>
2021-08-23 10:21:26 +0500

    Initial Commit

---
+++ Instagram_Clon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram_Clon
+//
+//  Created by Apple user on 23/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
